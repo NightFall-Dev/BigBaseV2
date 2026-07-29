2d4c22e4b1239076dd18364668bf9fa97cc4dea6
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2026-07-30 07:48:52 +0800

    More fixes
    - Improved session multiplexer
    - Fixed kick from interior
    - Fixed explode player
    - Fixed force script host
    - Re-added null function kick
    - Re-added auto kick host
    - Removed host token spoofing

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/NightFall-Dev/GTAV-Classes.git
-    GIT_TAG        fa950fb00c52fc437b27ae9f7aa8f1d245af8449
+    GIT_TAG        91d4c2c101bd308b987c0cee5cc1f9dee671944e
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
