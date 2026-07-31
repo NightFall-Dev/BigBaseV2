91023427cce78f25958c24f8d91f7aaadf6803bb
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2026-08-01 01:42:42 +0800

    Update for b3717

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/NightFall-Dev/GTAV-Classes.git
-    GIT_TAG        13b2deced5f8194827f42c2ea75ccf1e25df4004
+    GIT_TAG        ed0b9e9269913b37e8fefa9b16d67ac7430a3514
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
